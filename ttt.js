aeae74c37ff42eda1175614b2afe224b8764486b
black-hulk <rajibsadhak8@gmail.com>
2023-10-12 14:25:36 +0600

    'Tic-Tac-Toe'

--- ttt.js
+++ ttt.js
@@ -131,7 +131,7 @@ function GameReset(){
     document.getElementById('b9').value='';
     
 }
-// checking Winner the game
+// checking win or tie the game
 function winner(b1,b2,b3,b4,b5,b6,b7,b8,b9){
     b1=document.getElementById('b1').value;
     b2=document.getElementById('b2').value;
@@ -142,11 +142,122 @@ function winner(b1,b2,b3,b4,b5,b6,b7,b8,b9){
     b7=document.getElementById('b7').value;
     b8=document.getElementById('b8').value;
     b9=document.getElementById('b9').value;
-    if((b1=='X' || b1=='x') && (b2=='X'||b2=='x') && (b3=='X'||b3=='x')){
-        document.getElementById('printMessage').innerHTML='player x is win';
-        console.log("win");
+    if(((b1=='X') && (b2=='X') && (b3=='X'))||((b1=='0') && (b2=='0') && (b3=='0'))){
+        document.getElementById('printMessage').innerHTML='Player ' + document.getElementById('b2').value + ' is win';
+        //mark box for win
         document.getElementById('b1').style.color='red';
         document.getElementById('b2').style.color='red';
         document.getElementById('b3').style.color='red';
+        //disable others boxes
+        document.getElementById('b4').disabled=true;
+        document.getElementById('b5').disabled=true;
+        document.getElementById('b6').disabled=true;
+        document.getElementById('b7').disabled=true;
+        document.getElementById('b8').disabled=true;
+        document.getElementById('b9').disabled=true;
+    }
+    else if(((b4=='X') && (b5=='X') && (b6=='X')||(b4=='0') && (b5=='0') && (b6=='0'))){
+        document.getElementById('printMessage').innerHTML='Player ' + document.getElementById('b5').value + ' is win';
+        //mark box for win
+        document.getElementById('b4').style.color='red';
+        document.getElementById('b5').style.color='red';
+        document.getElementById('b6').style.color='red';
+        //disable others boxes
+        document.getElementById('b1').disabled=true;
+        document.getElementById('b2').disabled=true;
+        document.getElementById('b3').disabled=true;
+        document.getElementById('b7').disabled=true;
+        document.getElementById('b8').disabled=true;
+        document.getElementById('b9').disabled=true;
+    }
+    else if(((b7=='X') && (b8=='X') && (b9=='X')||(b7=='0') && (b8=='0') && (b9=='0'))){
+        document.getElementById('printMessage').innerHTML='Player ' + document.getElementById('b8').value + ' is win';
+        //mark box for win
+        document.getElementById('b7').style.color='red';
+        document.getElementById('b8').style.color='red';
+        document.getElementById('b9').style.color='red';
+        //disable others boxes
+        document.getElementById('b1').disabled=true;
+        document.getElementById('b2').disabled=true;
+        document.getElementById('b3').disabled=true;
+        document.getElementById('b4').disabled=true;
+        document.getElementById('b5').disabled=true;
+        document.getElementById('b6').disabled=true;
+    }
+    else if(((b3=='X') && (b5=='X') && (b7=='X')||(b3=='0') && (b5=='0') && (b7=='0'))){
+        document.getElementById('printMessage').innerHTML='Player ' + document.getElementById('b5').value + ' is win';
+        //mark box for win
+        document.getElementById('b3').style.color='red';
+        document.getElementById('b5').style.color='red';
+        document.getElementById('b7').style.color='red';
+        //disable others boxes
+        document.getElementById('b1').disabled=true;
+        document.getElementById('b2').disabled=true;
+        document.getElementById('b4').disabled=true;
+        document.getElementById('b6').disabled=true;
+        document.getElementById('b8').disabled=true;
+        document.getElementById('b9').disabled=true;
+    }
+    else if(((b1=='X') && (b5=='X') && (b9=='X')||(b1=='0') && (b5=='0') && (b9=='0'))){
+        document.getElementById('printMessage').innerHTML='Player ' + document.getElementById('b5').value + ' is win';
+        //mark box for win
+        document.getElementById('b1').style.color='red';
+        document.getElementById('b5').style.color='red';
+        document.getElementById('b9').style.color='red';
+        //disable others boxes
+        document.getElementById('b2').disabled=true;
+        document.getElementById('b3').disabled=true;
+        document.getElementById('b4').disabled=true;
+        document.getElementById('b6').disabled=true;
+        document.getElementById('b7').disabled=true;
+        document.getElementById('b8').disabled=true;
+    }
+    else if(((b1=='X') && (b4=='X') && (b7=='X')||(b1=='0') && (b4=='0') && (b7=='0'))){
+        document.getElementById('printMessage').innerHTML='Player ' + document.getElementById('b4').value + ' is win';
+        //mark box for win
+        document.getElementById('b1').style.color='red';
+        document.getElementById('b7').style.color='red';
+        document.getElementById('b4').style.color='red';
+        //disable others boxes
+        document.getElementById('b5').disabled=true;
+        document.getElementById('b2').disabled=true;
+        document.getElementById('b3').disabled=true;
+        document.getElementById('b6').disabled=true;
+        document.getElementById('b8').disabled=true;
+        document.getElementById('b9').disabled=true;
+    }
+    else if(((b2=='X') && (b5=='X') && (b8=='X')||(b2=='0') && (b5=='0') && (b8=='0'))){
+        document.getElementById('printMessage').innerHTML='Player ' + document.getElementById('b5').value + ' is win';
+        //mark box for win
+        document.getElementById('b2').style.color='red';
+        document.getElementById('b5').style.color='red';
+        document.getElementById('b8').style.color='red';
+        //disable others boxes
+        document.getElementById('b1').disabled=true;
+        document.getElementById('b3').disabled=true;
+        document.getElementById('b4').disabled=true;
+        document.getElementById('b6').disabled=true;
+        document.getElementById('b7').disabled=true;
+        document.getElementById('b9').disabled=true;
+    }
+    else if(((b3=='X') && (b6=='X') && (b9=='X')||(b3=='0') && (b6=='0') && (b9=='0'))){
+        document.getElementById('printMessage').innerHTML='Player ' + document.getElementById('b6').value + ' is win';
+        //mark box for win
+        document.getElementById('b3').style.color='red';
+        document.getElementById('b6').style.color='red';
+        document.getElementById('b9').style.color='red';
+        //disable others boxes
+        document.getElementById('b1').disabled=true;
+        document.getElementById('b2').disabled=true;
+        document.getElementById('b4').disabled=true;
+        document.getElementById('b5').disabled=true;
+        document.getElementById('b7').disabled=true;
+        document.getElementById('b8').disabled=true;
+    }
+    //if tie the game
+    else if((b1=='X'||b1=='0')&&(b2=='X'||b2=='0')&&(b3=='X'||b3=='0')&&(b4=='X'||b4=='0')
+            &&(b5=='X'||b5=='0')&&(b6=='X'||b6=='0')&&(b7=='X'||b7=='0')
+            &&(b8=='X'||b8=='0')&&(b9=='X'||b9=='0')){
+                document.getElementById('printMessage').innerHTML='The game is tie !';
     }
 }
